d52f71e8f7bfe1d1280f9f3b1344853c5c210f6d
Chris Sayen <chrissayen@iMac.local>
2020-04-13 18:30:55 -0400

    switched to async-redis to reduce callback madness

--- react/src/index.js
+++ react/src/index.js
@@ -38,9 +38,8 @@ var SOCKETS = new Map(); // TODO: put in redis
 io.on("connection", socket => {
 
   socket.on("newGame", async (room, player, callback) => {
-    console.log("newGame event")
-    let exists = await client.exists(`${room}:game`)
-    if (exists) {
+    console.log("newGame", room, player)
+    if (await client.exists(`${room}:game`)) {
       callback(false);
     } else {
       await client.hmset(
@@ -54,9 +53,13 @@ io.on("connection", socket => {
         "started", 0
       )
       addPlayer(room, player, false, socket, async (success) => {
-        let len = await client.rpush('openGames', room)
-        console.log("new game:", room)
-        callback(true)
+        if (success) {
+          client.rpush('openGames', room)
+          console.log("new game:", room)
+          callback(true)
+          return
+        }
+        callback(false)
       })
     }
   })
@@ -78,7 +81,8 @@ io.on("connection", socket => {
   socket.on("getOpenGames", getOpenGames)
 
   socket.on("joinGame", async (room, player, callback) => {
-    if (await client.exists(room)) {
+    console.log("joinGame", room, player)
+    if (await client.exists(`${room}:game`)) {
       let [isPrivate, started, hash] = await client.hmget(
         `${room}:game`,
         "isPrivate",
@@ -92,55 +96,54 @@ io.on("connection", socket => {
   });
 
   socket.on("startGame", (room, callback) => {
+    console.log("startGame", room)
     nextRound(room);
     callback(true);
   });
 
   socket.on("chooseWinner", card => {
+    console.log("chooseWinner", card)
     io.to(socket.rooms[0]).emit("winnerSelected", card);
     nextRound(socket.rooms[0]);
   });
 
   socket.on("playWhiteCard", async (card, player, callback) => {
+    console.log("chooseWhiteCard", card, player)
     const room = Object.keys(socket.rooms)[1];
-    getPlayerCards(room, player, cards => {
-      console.log("preFiltrer", cards)
-      setPlayerCards(room, player, cards.filter(c => c.text != card.text), (cards) => {
-        console.log("postFilter", cards)
-      })
-      let numberOfCardsPlayed = await client.hincrby(`${room}:game`, "numberOfCardsPlayed", 1)
-      let pick = await client.hget(`${room}:game`, "pick")
-      let playedCardsString = await client.hget(`${room}:${player}`, "playedCards")
-      let playedCardsJson = JSON.parse(playedCardsString)
-      let playedCards = playedCardsJson.length
-        ? playedCardsJson.map(string => JSON.parse(string)).push(card)
-        : [card]
-      if (pick == playedCards.length) {
-        io.to(room).emit(
-          "whiteCardPlayed",
-          playedCards.map(_card => ({
-            card: _card,
-            user: player
-          }))
-        )
-        client.hset(`${room}:${player}`, "playedCards", "[]")
-      } else {
-        client.hset(
-          `${room}:${player}`,
-          "playedCards",
-          JSON.stringify([playedCards]),
-          handleRedisError
-        )
-      }
-      let numPlayers = await client.llen(`${room}:players`)
-      if (err) console.error(err);
-      if (numberOfCardsPlayed == (numPlayers - 1) * parseInt(pick)) {
-        io.to(room).emit("allCardsPlayed");
-        client.hset(`${room}:game`, "numberOfCardsPlayed", 0)
-      }
-      callback(true);
+    let cards = await getPlayerCards(room, player)
+    console.log("preFiltrer", cards)
+    setPlayerCards(room, player, cards.filter(c => c.text != card.text), (cards) => {
+      console.log("postFilter", cards)
+    })
+    let numberOfCardsPlayed = await client.hincrby(`${room}:game`, "numberOfCardsPlayed", 1)
+    let pick = await client.hget(`${room}:game`, "pick")
+    let playedCardsString = await client.hget(`${room}:${player}`, "playedCards")
+    let playedCardsJson = JSON.parse(playedCardsString)
+    let playedCards = playedCardsJson.length
+      ? playedCardsJson.map(string => JSON.parse(string)).push(card)
+      : [card]
+    if (pick == playedCards.length) {
+      io.to(room).emit(
+        "whiteCardPlayed",
+        playedCards.map(_card => ({
+          card: _card,
+          user: player
+        }))
+      )
+      client.hset(`${room}:${player}`, "playedCards", "[]")
+    } else {
+      client.hset(
+        `${room}:${player}`,
+        "playedCards",
+        JSON.stringify([playedCards])
+      )
     }
-    );
+    let numPlayers = await client.llen(`${room}:players`)
+    if (numberOfCardsPlayed == (numPlayers - 1) * parseInt(pick)) {
+      io.to(room).emit("allCardsPlayed");
+      client.hset(`${room}:game`, "numberOfCardsPlayed", 0)
+    }
+    callback(true);
   });
 
   socket.on("ready", async ready => {
@@ -156,11 +159,11 @@ io.on("connection", socket => {
 
   socket.on("chooseWhiteCard", async (data, callback) => {
     const room = Object.keys(socket.rooms)[1]
-    let numberOfChoseCards =
+    let numberOfChosenCards =
       await client.lpush(`${room}:winningCards`, JSON.stringify(data.card))
     let pick = await client.hget(`${room}:game`, "pick")
-    if (numberOfChoseCards == parseInt(pick)) {
-      let chosenCardsStrings = client.lrange(`${room}:winningCards`, 0, -1)
+    if (numberOfChosenCards == parseInt(pick)) {
+      let chosenCardsStrings = await client.lrange(`${room}:winningCards`, 0, -1)
       let chosenCards = chosenCardsStrings.map(s => JSON.parse(s))
       io.to(room).emit("winnerSelected", data.user, chosenCards)
       client.del(`${room}:winningCards`)
@@ -168,14 +171,19 @@ io.on("connection", socket => {
   })
 
   socket.on("disconnect", async () => {
-    let nameRoom = await client.hmget(`${socket.id}`, 'name', 'room')
-    let [name, room] = nameRoom
-    await client.lrem(`${room}:players`, 1, name)
+    let [name, room] = await client.hmget(`${socket.id}`, 'name', 'room')
+
+    if (await client.llen(`${room}:players`)) {
+      client.lrem(`${room}:players`, 1, name)
+      console.log("removing player:", name)
+    }
+
     await client.del(`${room}:${name}`)
-    console.log("removing player:", name)
     SOCKETS.delete(name)
     let players = await client.lrange(`${room}:players`, 0, -1)
+    // If any players remain, update the room with the new list
     if (players.length) {
+      // Array.from(new Set(...)) is used to remove duplicates
       io.to(room).emit("updatePlayers",
         Array.from(
           new Set(
@@ -183,10 +191,12 @@ io.on("connection", socket => {
           )
         )
       )
-    } else {
+    }
+    else {
       client.del(`${room}:game`)
       client.del(`${room}:players`)
     }
+
   })
 })
 
@@ -199,7 +209,7 @@ const getOpenGames = async (callback) => {
     }
   }
   let games = await client.lrange("openGames", 0, -1)
-  games.forEach(game => {
+  games.forEach(async game => {
   let players = await client.lrange(`${game}:players`, 0, -1)
     compileGames(game, players.length, games.length, callback)
   })
@@ -208,16 +218,17 @@ const getOpenGames = async (callback) => {
 const addPlayer = async (room, player, started, socket, callback) => {
   console.log("adding:", player, "room:", room);
   let len = await client.llen(`${room}:players`)
+
   if (len > MAX_PLAYERS) {
     client.rpop(`${room}:players`)
     callback(false);
-  } else {
+  }
+  else {
     client.rpush(`${room}:players`, player)
-    client.hset(`${room}:${player}`, "playedCards", "[]");
-    client.hset(`${room}:${player}`, "cards", "[]");
+    client.hmset(`${room}:${player}`, "playedCards", "[]", "cards", "[]");
     client.hmset(`${socket.id}`, 'name', player, 'room', room)
     SOCKETS.set(player, socket);
-    socket.join(room, err => {
+    socket.join(room, async () => {
       dealWhiteCards(room, player)
       let players = await client.lrange(`${room}:players`, 0, -1)
       io.to(room).emit(
@@ -232,28 +243,25 @@ const addPlayer = async (room, player, started, socket, callback) => {
   }
 };
 
-const nextCzar = async (room, callback) => {
+const nextCzar = async (room) => {
   let czar = await client.lpop(`${room}:players`)
-  handleRedisError(err);
-  client.rpush(`${room}:players`, czar, handleRedisError);
-  callback(czar);
+  client.rpush(`${room}:players`, czar);
+  return czar
 };
 
-const _drawCards = async (room, color, n, callback) => {
+const _drawCards = async (room, color, n) => {
   let cursor = await client.hincrby(`${room}:game`, `${color}Cursor`, n)
-  handleRedisError(err);
-  callback(DECK[color].slice(cursor - n + 1, cursor + 1));
-};
+  return DECK[color].slice(cursor - n + 1, cursor + 1)
+}
 
-const drawCards = (filterfn, room, color, n, callback) => {
-  let f = (newN, oldCards) => {
-    _drawCards(room, color, newN, newCards => {
-      let filtered = oldCards.concat(newCards.filter(filterfn));
-      if (filtered.length < newN) return f(newN - filtered.length, filtered)
-      callback(filtered);
-    });
+const drawCards = async (filterfn, room, color, n) => {
+  let f = async (newN, oldCards) => {
+    let newCards = await _drawCards(room, color, newN)
+    let filtered = oldCards.concat(newCards.filter(filterfn));
+    if (filtered.length < newN) return f(newN - filtered.length, filtered)
+    return filtered
   };
-  f(n, []);
+  return f(n, []);
 };
 
 const wfilter = (card) => !card.text.includes('*')
@@ -263,49 +271,39 @@ const setPlayerCards = (room, player, cards, callback) => {
   client.hset(`${room}:${player}`, "cards", JSON.stringify(cards))
 }
 
-const getPlayerCards = async (room, player, callback) => {
+const getPlayerCards = async (room, player) => {
   let r = await client.hget(`${room}:${player}`, "cards")
-  callback
-    ? callback(JSON.parse(r))
-    : null
+  return JSON.parse(r)
 }
 
-const dealWhiteCards = (room, player, callback) => {
-  getPlayerCards(room, player, (cards) => {
-    console.log("player cards", cards)
-    drawCards(wfilter, room, "white", 7 - cards.length, (newCards) => {
-      console.log("newCards", newCards)
-      let cardsDealt = [...cards, ...newCards]
-      setPlayerCards(room, player, cardsDealt)
-      SOCKETS.get(player).emit("dealWhite", cardsDealt)
-      callback ? callback(cards) : null
-    })
-  })
+const dealWhiteCards = async (room, player, callback) => {
+  console.log("dealWhiteCards", room, player)
+  let cards = await getPlayerCards(room, player)
+  console.log("player cards", cards)
+  let newCards = await drawCards(wfilter, room, "white", 7 - cards.length)
+  console.log("newCards", newCards)
+  let cardsDealt = [...cards, ...newCards]
+  setPlayerCards(room, player, cardsDealt)
+  SOCKETS.get(player).emit("dealWhite", cardsDealt)
+  callback ? callback(cards) : null
 }
 
-const dealBlackCard = (room, callback) => {
-  drawCards(bfilter, room, "black", 1, (_black) => {
-    const black = _black[0]
-    client.hset(`${room}:game`, 'pick', black.pick)
-    io.to(room).emit('dealBlack', black)
-    callback ? callback(black) : null
-  })
+const dealBlackCard = async (room, callback) => {
+  console.log("dealBlackCard", room)
+  let _black = await drawCards(bfilter, room, "black", 1)
+  const black = _black[0]
+  client.hset(`${room}:game`, 'pick', black.pick)
+  io.to(room).emit('dealBlack', black)
+  callback ? callback(black) : null
 }
 
 const nextRound = async (room) => {
   let players = await client.lrange(`${room}:players`, 0, -1)
-  nextCzar(room, (czar) => {
-    io.to(room).emit('newCzar', czar)
-    dealBlackCard(room)
-    players.forEach(player => {
-      SOCKETS.get(player).emit("myTurn", (player == czar))
-      dealWhiteCards(room, player)
-    })
+  let czar = await nextCzar(room)
+  io.to(room).emit('newCzar', czar)
+  dealBlackCard(room)
+  players.forEach(player => {
+    SOCKETS.get(player).emit("myTurn", (player == czar))
+    dealWhiteCards(room, player)
   })
-}
-
-const handleRedisError = (err) => {
-  if (err instanceof Error) {
-    console.error(err)
-  }
 }
